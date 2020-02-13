0d5609fa9f6add8df9a11fc4f06a6c20fafde1d3
WingSMC <gergely089@gmail.com>
2020-02-13 14:57:23 +0100

    nav almost complete(menu items in mobile)

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,11 @@ window.addEventListener("load", init);
 
 function init() {
     buttonActive();
+    toggleNav();
 }
 
-function buttonActive() {    
-    let elements = document.querySelectorAll(".navs button");
+function buttonActive() {
+    let elements = document.querySelectorAll("#navs button");
     let current = elements[0];
     elements.forEach(elem => {
         elem.addEventListener("click", () => {
@@ -14,5 +15,18 @@ function buttonActive() {
             current = elem;
             current.classList.add("active");
         });
+    });
+}
+
+function toggleNav() {
+    let hidden = true;
+    let navs = document.getElementById("navs");
+    document.getElementById("hamburger")
+    .addEventListener("click", () => {
+        hidden = !hidden;
+        hidden ?
+            navs.classList.add("hide") :
+            navs.classList.remove("hide")
+        ;
     });
 }
